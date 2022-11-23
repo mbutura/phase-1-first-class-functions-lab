fc3b3059802814baa111571bbe92bd9e37cdb17a
Alois Mbutura <alois.mbutura@student.moringaschool.com>
2022-11-23 14:12:24 +0000

    Stuck at tests

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(arr){
+    return arr.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(arr){
+    return arr.slice(-2);
+}
+
+selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+console.log(typeof selectingDrivers)
+
+const createFareMultiplier = function(num){
+    return function(fare){return num*fare};
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arr, cb){
+    return cb(arr)
+}
